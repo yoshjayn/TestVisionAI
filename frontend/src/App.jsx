4b54a1294e9bdebd54bdc3ef5003b67872b6d7e4
Yashaswini <aswiniy68@gmail.com>
2024-09-24 21:43:10 +0530

    corrected the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import { Route, Routes } from 'react-router-dom';
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Sign/>} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Sign/>} />
+      <Route path="/" element={<Home />} />
       <Route path="/test-vision" element={<TestVision />} />
     </Routes>
   );
